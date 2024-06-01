eebe3922b3bfda169f1b8b5f58c8f47362929ec2
Maryam <maryamjaved.xo@gmail.com>
2024-06-01 17:59:34 +0500

    my orders

--- src/screens/MyOrder.js
+++ src/screens/MyOrder.js
@@ -45,12 +45,9 @@ export default function MyOrder() {
                   ) : (
                     <div className='col-12 col-md-6 col-lg-3'>
                       <div className="card mt-3" style={{ width: "16rem", maxHeight: "360px" }}>
-                        <img src={item.img} className="card-img-top" alt="..." style={{ height: "120px", objectFit: "fill" }} />
                         <div className="card-body">
                           <h5 className="card-title">{item.name}</h5>
                           <div className='container w-100 p-0' style={{ height: "38px" }}>
-                            <span className='m-1'>Quantity: {item.quantity}</span>
-                            <span className='m-1'>Size: {item.size}</span>
                             <div className='d-inline ms-2 h-100 w-20 fs-5'>
                               ₹{item.price}/-
                             </div>
